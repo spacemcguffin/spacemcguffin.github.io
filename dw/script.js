d08a7d9f3d629b3e1be34a50afabe8ba22b71135
spacemcguffin <87235845+spacemcguffin@users.noreply.github.com>
2025-08-02 14:36:22 +0100

    Update script.js

--- dw/script.js
+++ dw/script.js
@@ -116,3 +116,32 @@ document.querySelectorAll(".link-section .dropdown")?.forEach((dropdown) => {
     }
   });
 });
+
+
+
+
+
+
+
+
+// Place any jQuery/helper plugins in here.
+
+$(document).ready(function(){
+	$(".contentsPanel").each(function() {                
+		$(this).prepend('<div class="hidePanel">[hide]</div><div class="showPanel">[show]</div>');
+	});
+	
+	
+    $(".hidePanel").click(function(){
+		$( this ).siblings('ul').hide( 150, function() { 
+			$(this).parent().addClass('minimizedPanel');
+		});
+    });
+    $(".showPanel").click(function(){
+		$( this ).siblings('ul').show( 150, function() { 
+			$(this).parent().removeClass('minimizedPanel');
+		});
+    });
+	
+	
+});
