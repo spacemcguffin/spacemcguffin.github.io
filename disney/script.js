e30f96fdba92958f88777ff05b770fe3bf5a6942
spacemcguffin <87235845+spacemcguffin@users.noreply.github.com>
2025-12-21 18:25:19 +0000

    Update script.js

--- disney/script.js
+++ disney/script.js
@@ -1,6 +1,6 @@
 let movies = [
   {
-    name: "Twisted Christian",
+    name: "<a href="https://google.com">Twisted Christian</a>",
     season: "Season 28, Episode 1",
     des: "Cartman is possessed and may be the key to stopping the Antichrist.",
     image: "/wiki/img/episodes/tc.png",
