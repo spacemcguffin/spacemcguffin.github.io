9352db3c16151eb854a8e3eca3d516fbf54c2412
spacemcguffin <87235845+spacemcguffin@users.noreply.github.com>
2025-12-21 14:38:20 +0000

    Update script.js

--- disney/script.js
+++ disney/script.js
@@ -82,20 +82,6 @@ setInterval(() => {
   createSlide();
 }, 5000);
 
-// video cards
-
-const videoCards = document.querySelectorAll(".video-card");
-
-videoCards.forEach((item) => {
-  item.addEventListener("mouseover", () => {
-    let video = item.children[1];
-    video.play();
-  });
-  item.addEventListener("mouseleave", () => {
-    let video = item.children[1];
-    video.pause();
-  });
-});
 
 // cards sliders
 
